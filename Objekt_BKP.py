fbf8684a20d41e62e856576ba0a71c0763cf9c9e
Jakob <jaka,zarnik@gmail.com>
2021-01-09 14:06:52 +0100

    Update Objekt_BKP.py
    Groba osnova za nadaljno izdelavo objekta.

--- Objekt_BKP.py
+++ Objekt_BKP.py
@@ -1,8 +1,21 @@
 class BKP:
 
-    def __init__(self, teze, s_prihodek, i_prihodek, kapital, optimisticen = True):
-        self.teze = utezi
-        self.s_prihodek = koef1
-        self.s_prihodek = koef2
-        self.kapital = y
+    def __init__(self, teze, s_prihodek, i_prihodek, kapital, optimisticen):
+        self.teze = teze
+        self.s_prihodek = s_prihodek
+        self.i_prihodek = i_prihodek
+        self.kapital = kapital
+        self.optimisticen = True
+
+    def optimalna_resitev(self):
+        print("resitev, ki jo vrne funkcija") #uporabimo funkcijo v BKP.py
+
+    def casovna_zahtevnost(self):
+        print("koliko casa porabi algoritem")
+
+bkp1 = BKP([2, 4, 1, 8], [3, 3, 2, 2], [1, 2, 3, 4], 8, True)
+
+print(bkp1.kapital)
+print(bkp1.casovna_zahtevnost)
+
         
