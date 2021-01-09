02470c1c11f5ff7faa666843be1f8484c410d0b5
Jakob <jaka,zarnik@gmail.com>
2021-01-09 13:04:35 +0100

    Create Generiranje_podatkov.py
    Generiranje nakljucni podatkov za testiranje algoritma

---
+++ Generiranje_podatkov.py
@@ -0,0 +1,22 @@
+import random
+
+y = 32 #maximalna velikost nahrbtnika, ki jo vodja lahko izbere (~omejitev kapitala)
+st_predmetov = [10, 25, 50, 75, 100] #koliko predmetov je na izbiro 
+
+koef1 = [] #koeficienti vodje (seznam koef. koristnosti za posamezen predmet)
+koef2 = [] #koeficienti sledilca (seznam koef. koristnosti za posamezen predmet)
+utezi_predmetov = [] #utezi predmetov, pri izbiri jih omejuje kapaciteta "y"
+for n in st_predmetov:
+    for i in range(0, n):
+        p = random.randint(1, 30)
+        koef1.append(p)
+    print(koef1)
+    for i in range(0, n):
+        p = random.randint(1, 30)
+        koef2.append(p)
+    print(koef1)
+    for i in range(0, n):
+        p = random.randint(1, y)
+        utezi_predmetov.append(p)
+    print(utezi_predmetov)
+    print("")
