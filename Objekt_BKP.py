cb4b6dc9ec22eea4bb09c50cf14a6019be9dfc7a
Jakob <jaka,zarnik@gmail.com>
2021-01-09 13:40:47 +0100

    Objekt

---
+++ Objekt_BKP.py
@@ -0,0 +1,8 @@
+class BKP:
+
+    def __init__(self, teze, s_prihodek, i_prihodek, kapital, optimisticen = True):
+        self.teze = utezi
+        self.s_prihodek = koef1
+        self.s_prihodek = koef2
+        self.kapital = y
+        
