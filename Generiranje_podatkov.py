b2325efa7f8c009c2ac00b948979cac2a454b6f0
Tin Markon <tin.markon@stroka.si>
2021-01-10 13:51:11 +0100

    Dodano veckratno merjenje izracuna in priprava grafov

--- Generiranje_podatkov.py
+++ Generiranje_podatkov.py
@@ -1,8 +1,10 @@
 import random
 from time import perf_counter
 from BKP import BKP
+from statistics import variance, mean
+import matplotlib.pyplot as plt
 
-st_predmetov = [10, 25, 50, 75, 100] #V seznamu dolocis stevilo predmetov za problem
+st_predmetov = [10, 25, 50, 75, 100, 500, 1000] #V seznamu dolocis stevilo predmetov za problem
 st_ponovitev = 100 ### Kolikokrat ponovimo izracun za vsako stevilo predmetov
 
 def gen_n(n, y): #generiranje seznamov z n predmeti, y ~ max volumen
@@ -21,15 +23,25 @@ def gen_n(n, y): #generiranje seznamov z n predmeti, y ~ max volumen
     return koef1, koef2, utezi_predmetov, y
 
 seznam_casov = []
-for i in st_predmetov:
+for i in st_predmetov: ### For zanka za vsako stevilo predmetov
     fiksni_seznam = []
-    for j in range(st_ponovitev):
+    for j in range(st_ponovitev): ### Fiksiramo stevilo predmetov in merimo kako dolgo potrebujemo za objekt 
         koef1, koef2, utezi, kapital = gen_n(i, 40)
         zacetek = perf_counter()
         bkp = BKP(utezi, koef1, koef2, 0, kapital, -1, True)
         bkp.optimalna_resitvev()
         konec = perf_counter()
         fiksni_seznam.append(konec - zacetek)
-    seznam_casov.append(fiksni_seznam)
+    seznam_casov.append((mean(fiksni_seznam), variance(fiksni_seznam)))
 
-print(len(seznam_casov[0]))        
+
+y = [seznam_casov[i][0] for i in range(len(st_predmetov))] ### Seznam y koordinat - povprecja
+var = [seznam_casov[i][1] for i in range(len(st_predmetov))] ### seznam y koordinat - varianca
+
+plt.plot(st_predmetov, y, 'o', st_predmetov, var, 'x')
+plt.show()
+
+
+
+
+    
