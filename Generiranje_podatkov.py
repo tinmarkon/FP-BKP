598402d71309fe77a532bca615f394a49b1656fa
Jakob <jaka,zarnik@gmail.com>
2021-01-10 21:59:55 +0100

    parameter gen_n
    Dodal parameter za omejitev max utezi predmetov

--- Generiranje_podatkov.py
+++ Generiranje_podatkov.py
@@ -1,20 +1,20 @@
 import random
+import matplotlib.pyplot as plt
 from time import perf_counter
 from BKP import BKP
 from statistics import variance, mean
-import matplotlib.pyplot as plt
 
 st_predmetov = [10, 25, 50, 75, 100, 500, 1000] #V seznamu dolocis stevilo predmetov za problem
 st_ponovitev = 100 ### Kolikokrat ponovimo izracun za vsako stevilo predmetov
 
-def gen_n(n, y): #generiranje seznamov z n predmeti, y ~ max volumen
+def gen_n(n, y, m): #generiranje seznamov z n predmeti, y ~ max volumen, m ~ max omejitev utezi predmetov
     koef1 = []
     koef2 = []
     utezi_predmetov = [] #utezi predmetov, pri izbiri jih omejuje kapaciteta "y"
     for i in range(0, n):
-        p = random.randint(1, 30)
+        p = random.randint(1, m)
         koef1.append(p)
-        r = random.randint(1, 30)
+        r = random.randint(1, m)
         koef2.append(r)
         k = random.randint(1, y)
         # tako dosezemo, da je vsak predmet lahko vsebovan v nahhrbtniku
@@ -26,7 +26,7 @@ seznam_casov = []
 for i in st_predmetov: ### For zanka za vsako stevilo predmetov
     fiksni_seznam = []
     for j in range(st_ponovitev): ### Fiksiramo stevilo predmetov in merimo kako dolgo potrebujemo za objekt 
-        koef1, koef2, utezi, kapital = gen_n(i, 40)
+        koef1, koef2, utezi, kapital = gen_n(i, 40, 32)
         zacetek = perf_counter()
         bkp = BKP(utezi, koef1, koef2, 0, kapital, -1, True)
         bkp.optimalna_resitvev()
