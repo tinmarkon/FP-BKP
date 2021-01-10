8ecfffc7e9ea9ecb6de2a0cab78e69808b78cd7a
Tin Markon <tin.markon@stroka.si>
2021-01-10 12:27:02 +0100

    Dodani dodatni primeri

--- BKP.py
+++ BKP.py
@@ -86,4 +86,6 @@ class BKP:
         return predmeti[::-1], optKapital
 
 ### PRIMERI ###
+#bkp1 = BKP([2, 4, 1, 8], [3, 3, 2, 2], [1, 2, 3, 4], 0, 8, -2, True)
+#bkp2 = BKP([1, 2, 3, 4], [4,5,10,15], [5,1,1,1], 1, 4, -1, True)
 #bkp3 = BKP([5,3,2,1], [1, 1, 1, 1], [3,5,1,9], 0, 6, -2, True)
