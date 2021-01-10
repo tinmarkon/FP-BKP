265429b5aee8bf25a426be8105a07b76f4e320d3
Tin Markon <tin.markon@stroka.si>
2021-01-10 12:23:00 +0100

    Združevanje dveh datotek, spremenjena imena

--- BKP.py
+++ BKP.py
@@ -1,73 +1,86 @@
-def generiranjeTabel(teze, s_prihodek, i_prihodek, kapital, optimisticen = True):
-    sledilec = list()
-    investitor = list()
-    zacetni_predmet_s, zacetni_predmet_i = prvi_predmeti(kapital, s_prihodek, i_prihodek, teze)
-    sledilec.append(zacetni_predmet_s)
-    investitor.append(zacetni_predmet_i)
-    for k in range(1, len(teze)):
-        fiksni_sledilec = list()
-        fiksni_investitor = list()
-        for j in range(0, kapital + 1): 
-            if (j < teze[k]):  
-               fiksni_investitor.append(investitor[-1][j])
-               fiksni_sledilec.append(sledilec[-1][j])
-            else: 
-                maksimalna_sledilec = max(sledilec[-1][j], sledilec[-1][j - teze[k]] + s_prihodek[k])
-                fiksni_sledilec.append(maksimalna_sledilec)
-                if (sledilec[-1][j] != sledilec[-1][j - teze[k]] + s_prihodek[k]):
-                    if fiksni_sledilec == sledilec[-1][j]:
-                        fiksni_investitor.append(investitor[-1][j])
-                    else: 
-                        fiksni_investitor.append(investitor[-1][j - teze[k]] + i_prihodek[k])  
+class BKP:
+    def __init__(self, teze, s_prihodek, i_prihodek, s_kapital, kapital, cena_enote, optimisticen):
+        self.teze = teze
+        self.s_prihodek = s_prihodek
+        self.i_prihodek = i_prihodek
+        self.s_kapital = s_kapital
+        self.kapital = kapital
+        self.cena_enote = cena_enote
+        self.optimisticen = True
 
+    def prvi_predmeti(self):
+        seznam_sledilec = list()
+        seznam_investitor = list()
+        for j in range(0, self.kapital + 1):
+            if (j >= self.teze[0]):
+                seznam_sledilec.append(self.s_prihodek[0])
+                seznam_investitor.append(self.i_prihodek[0])
+            else:
+                seznam_sledilec.append(0)
+                seznam_investitor.append(0)
+        return seznam_sledilec, seznam_investitor
+
+    def generiranje_tabel(self):
+        sledilec = list()
+        investitor = list()
+        zacetni_predmet_s, zacetni_predmet_i = self.prvi_predmeti()
+        sledilec.append(zacetni_predmet_s)
+        investitor.append(zacetni_predmet_i)
+        for k in range(1, len(self.teze)):
+            fiksni_sledilec = list()
+            fiksni_investitor = list()
+            for j in range(0, self.kapital + 1):
+                if (j < self.teze[k]):
+                    fiksni_investitor.append(investitor[-1][j])
+                    fiksni_sledilec.append(sledilec[-1][j])
                 else:
-                    if optimisticen:
-                        fiksni_investitor.append(max(investitor[-1][j], investitor[-1][j - teze[k]] + i_prihodek[k]))
+                    maksimalna_sledilec = max(
+                        sledilec[-1][j], sledilec[-1][j - self.teze[k]] + self.s_prihodek[k])
+                    fiksni_sledilec.append(maksimalna_sledilec)
+                    if (sledilec[-1][j] != sledilec[-1][j - self.teze[k]] + self.s_prihodek[k]):
+                        if fiksni_sledilec == sledilec[-1][j]:
+                            fiksni_investitor.append(investitor[-1][j])
+                        else:
+                            fiksni_investitor.append(
+                                investitor[-1][j - self.teze[k]] + self.i_prihodek[k])
                     else:
-                        fiksni_investitor.append(min(investitor[-1][j], investitor[-1][j - teze[k]] + i_prihodek[k]))
-        investitor.append(fiksni_investitor)
-        sledilec.append(fiksni_sledilec)
-    return investitor, sledilec
-    
-def prvi_predmeti(kapital, s_prihodek, i_prihodek, teze):
-    seznam_sledilec = list()
-    seznam_investitor = list()
-    for j in range(0, kapital + 1):
-        if (j >= teze[0]):
-            seznam_sledilec.append(s_prihodek[0])
-            seznam_investitor.append(i_prihodek[0])
-        else:
-            seznam_sledilec.append(0)
-            seznam_investitor.append(0)
-    return seznam_sledilec, seznam_investitor
+                        if self.optimisticen:
+                            fiksni_investitor.append(
+                                max(investitor[-1][j], investitor[-1][j - self.teze[k]] + self.i_prihodek[k]))
+                        else:
+                            fiksni_investitor.append(
+                                min(investitor[-1][j], investitor[-1][j - self.teze[k]] + self.i_prihodek[k]))
+            investitor.append(fiksni_investitor)
+            sledilec.append(fiksni_sledilec)
+        return investitor, sledilec
 
-def iskanjeResitvev(investitorjevaMatrika, sledilcevaMatrika, sKapital, kapital, cenaEnote, teze, i_prih, s_prih): 
-    invResitev = investitorjevaMatrika[-1] 
-    zasluzek = 0
-    optKapital = 0
-    vmesniKapital = 0
-    predmeti = list()
-    for i in range(sKapital, kapital + 1):  
-        if (zasluzek < invResitev[i] + i * cenaEnote): 
-            zasluzek = invResitev[i] + i * cenaEnote
-            optKapital = i
-            vmesniKapital = i
-    for j in range(len(sledilcevaMatrika) - 1, 0, -1):
-        if sledilcevaMatrika[j - 1][vmesniKapital] != sledilcevaMatrika[j - 1][vmesniKapital - teze[j]] + s_prih[j]:
-            if sledilcevaMatrika[j][vmesniKapital] == sledilcevaMatrika[j - 1][vmesniKapital]:
-                predmeti.append(0)
+    def iskanje_resitvev(self):
+        investitorjeva_matrika, sledilceva_matrika = self.generiranje_tabel()
+        invResitev = investitorjeva_matrika[-1]
+        zasluzek = 0
+        optKapital = 0
+        vmesniKapital = 0
+        predmeti = list()
+        for i in range(self.s_kapital, self.kapital + 1):
+            if (zasluzek < invResitev[i] + i * self.cena_enote):
+                zasluzek = invResitev[i] + i * self.cena_enote
+                optKapital = i
+                vmesniKapital = i
+        for j in range(len(sledilceva_matrika) - 1, 0, -1):
+            if sledilceva_matrika[j - 1][vmesniKapital] != sledilceva_matrika[j - 1][vmesniKapital - self.teze[j]] + self.s_prihodek[j]:
+                if sledilceva_matrika[j][vmesniKapital] == sledilceva_matrika[j - 1][vmesniKapital]:
+                    predmeti.append(0)
+                else:
+                    predmeti.append(1)
+                    vmesniKapital = vmesniKapital - self.teze[j]
             else:
-                predmeti.append(1)
-                vmesniKapital = vmesniKapital - teze[j]
+                if investitorjeva_matrika[j][vmesniKapital] == investitorjeva_matrika[j-1][vmesniKapital]:
+                    predmeti.append(0)
+                else:
+                    predmeti.append(0)
+                    vmesniKapital = vmesniKapital - self.teze[j]
+        if sledilceva_matrika[0][vmesniKapital] == 0:
+            predmeti.append(0)
         else:
-            if investitorjevaMatrika[j][vmesniKapital] == investitorjevaMatrika[j-1][vmesniKapital]:
-                predmeti.append(0)
-            else:
-                predmeti.append(0)
-                vmesniKapital = vmesniKapital - teze[j]
-    if sledilcevaMatrika[0][vmesniKapital] == 0:
-        predmeti.append(0)
-    else:
-        predmeti.append(1)
-    return predmeti[::-1], optKapital
-
+            predmeti.append(1)
+        return predmeti[::-1], optKapital
